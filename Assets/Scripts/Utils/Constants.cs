058decfe25dafb888259297db9ac8a17a1343a7b
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-22 21:11:23 +0200

    Add platform visual constant name definition
    Define PLATFORM_VISUAL_GAME_OBJECT_NAME ('Visual') inside Constants.cs for unified referencing of instantiated platform models.

--- Assets/Scripts/Utils/Constants.cs
+++ Assets/Scripts/Utils/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string BLUE_PLATFORM_TAG = "BluePlatform";
         public const string YELLOW_PLATFORM_TAG = "YellowPlatform";
+        public const string PLATFORM_VISUAL_GAME_OBJECT_NAME = "Visual";
     }
 }
