4d0a43400b4dfb0c267b38015010b3a3a9e77ef0
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-22 18:23:13 +0200

    Add core Constants definitions
    Create a utility class containing string constants for the newly registered BluePlatform and YellowPlatform tags.

---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Utils
+{
+    public static class Constants
+    {
+        public const string BLUE_PLATFORM_TAG = "BluePlatform";
+        public const string YELLOW_PLATFORM_TAG = "YellowPlatform";
+    }
+}
