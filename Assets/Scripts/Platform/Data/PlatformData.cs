659b1b52b357b3f63a4b4806e7115524a4199d7a
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-22 18:23:15 +0200

    Migrate PlatformColor definition and add PlatformData configurations
    Move the PlatformColor enum to the Game.Level namespace and under the Platform directory. In addition, introduce the PlatformData ScriptableObject class to configure platform visual prefabs.

---
+++ Assets/Scripts/Platform/Data/PlatformData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Level.Data
+{
+    [CreateAssetMenu(fileName = "New Platform Data", menuName = "Game/Level/Platforms", order = 0)]
+    public class PlatformData : ScriptableObject
+    {
+        public GameObject platformVisual;
+    }
+}
--- Assets/Scripts/Platform/PlatformColor.cs
+++ Assets/Scripts/Platform/PlatformColor.cs
@@ -1,4 +1,4 @@
-namespace Level
+namespace Game.Level
 {
     public enum PlatformColor
     {
