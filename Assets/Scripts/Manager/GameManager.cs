01e9fcee3ed0cd7c4e31dcd511288a6e39975fde
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-22 18:23:19 +0200

    Implement GameManager singleton scaffolding
    Introduce the GameManager singleton to provide persistent game-wide access to core controllers, including the active PlayerColorHandler.

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Game.Player;
+using UnityEngine;
+
+namespace Game.Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager m_instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if (m_instance != null) return m_instance;
+                
+                var go = new GameObject("GameManager");
+                m_instance = go.AddComponent<GameManager>();
+                DontDestroyOnLoad(go);
+                return m_instance;
+            }
+        }
+        
+        [SerializeField] private GameObject m_player;
+        private PlayerColorHandler m_playerColorHandler;
+        
+        public PlayerColorHandler PlayerColorHandler => m_playerColorHandler;
+    }
+}
