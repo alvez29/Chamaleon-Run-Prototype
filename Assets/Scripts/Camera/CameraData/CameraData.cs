ac9e6e38f3ffbd01172b9825662c408d518fcc24
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-23 12:49:17 +0200

    Introduce CameraData ScriptableObject configuration class
    Add the CameraData ScriptableObject defining camera variables: Offset, SmoothTime, and RotationInDegrees. Create the corresponding default CameraData asset under Assets/Data/Camera.

---
+++ Assets/Scripts/Camera/CameraData/CameraData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Camera
+{
+    [CreateAssetMenu(fileName = "New Camera Data", menuName = "Game/Camera", order = 0)]
+    public class CameraData : ScriptableObject
+    {
+        public Vector3 Offset = new(5f, 3f, -10f);
+        public Vector2 SmoothTime = new(0.1f, 0.1f);
+        public float RotationInDegrees = 30.0f; 
+    }
+}
