bb53b8e9e73792c31155d1d095310b81a73753df
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-24 22:54:00 +0200

    Implement TransitionManager and AnimatorSpeaker scripts
    Add TransitionManager to play FadeIn/FadeOut animations and TransitionManagerAnimatorSpeaker to dispatch events when fade animation clips complete. Clean up obsolete UI scripts folder.

---
+++ Assets/Scripts/Manager/TransitionManager/TransitionManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game.Level
+{
+    public class TransitionManager : MonoBehaviour
+    {
+        
+        [Header("Fade")]
+        [SerializeField] private Animator m_transitionAnimator;
+
+        public void FadeOut()
+        {
+            m_transitionAnimator.Play("FadeOut");
+        }
+
+        public void FadeIn()
+        {
+            m_transitionAnimator.Play("FadeIn");
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/TransitionManager/TransitionManagerAnimatorSpeaker.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Game.Manager.TransitionManager
+{
+    public class TransitionManagerAnimatorSpeaker : MonoBehaviour
+    {
+        public event System.Action OnFadeOutCompleted;
+        public event System.Action OnFadeInCompleted;
+
+        public void OnFadeOutAnimationCompleted()
+        {
+            OnFadeOutCompleted?.Invoke();
+        }
+    
+        public void OnFadeInAnimationCompleted()
+        {
+            OnFadeInCompleted?.Invoke();
+        }
+    }
+}
