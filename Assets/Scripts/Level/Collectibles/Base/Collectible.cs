6bea172932284bd08d2b918f58a7283f5d086c40
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-24 19:02:00 +0200

    Implement activation and deactivation state in Collectible base class
    Add state tracking inside the Collectible base class. Manage visual visibility via MeshRenderer toggles, invoke collection callbacks, and support full reactivation upon level reset.

--- Assets/Scripts/Level/Collectibles/Base/Collectible.cs
+++ Assets/Scripts/Level/Collectibles/Base/Collectible.cs
@@ -1,19 +1,48 @@
-﻿using Game.Utils;
+﻿using System;
+using Game.Utils;
 using UnityEngine;
 
 namespace Game.Level
 {
     public abstract class Collectible : MonoBehaviour
     {
+        public event Action<Collectible> OnCollectibleCollected;
+
+        private const int PLAYER_LAYER = 3;
+        
+        private MeshRenderer m_meshRenderer;
+        private bool m_isActivated = true;
+
+        private void Awake()
+        {
+            m_meshRenderer = GetComponentInChildren<MeshRenderer>();
+
+            Activate();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag(Constants.PLAYER_TAG))
+            if (m_isActivated && ((1 << other.gameObject.layer) & PLAYER_LAYER) == 0)
             {
-                OnCollected(other.gameObject);    
+                OnCollected(other.gameObject);
+                OnCollectibleCollected?.Invoke(this);
+                Deactivate();
             }
             
         }
 
         protected abstract void OnCollected(GameObject player);
+
+        private void Deactivate()
+        {
+            m_isActivated = false;
+            m_meshRenderer.enabled = false;
+        }
+
+        public void Activate()
+        {
+            m_isActivated = true;
+            m_meshRenderer.enabled = true;
+        }
     }
 }
--- Assets/Scripts/Level/Collectibles/ImpulseCollectible.cs
+++ Assets/Scripts/Level/Collectibles/ImpulseCollectible.cs
@@ -5,18 +5,13 @@ namespace Game.Level.Collectibles
     public class ImpulseCollectible : Collectible
     {
         [SerializeField] private float m_horizontalForceStrength = 3000;
-        
+
         protected override void OnCollected(GameObject other)
         {
-            other.TryGetComponent(out Rigidbody playerRigidBody);
-
-            if (playerRigidBody != null)
+            if (other.TryGetComponent(out Rigidbody playerRigidBody))
             {
-                Debug.Log("Impulse collectible collected!");
                 playerRigidBody.AddForce(new Vector3(m_horizontalForceStrength, 0.0f, 0.0f), ForceMode.Force);
-                Destroy(gameObject);
             }
-            
         }
     }
 }
