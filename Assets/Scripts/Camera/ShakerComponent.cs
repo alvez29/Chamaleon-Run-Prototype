7ed26ad1ac09c791dc1a99ffdf2503732059cdb1
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-25 18:26:12 +0200

    Implement screen shake, particle systems, and Kenney splash assets
    Create ShakerComponent to handle screen shakes using unscaled time. Implement PlayerFeedbackComponent to control camera shakes and colored particle bursts on high-speed landing or player death. Import Kenney splash texture assets to support particle visual effects.

---
+++ Assets/Scripts/Camera/ShakerComponent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.Camera
+{
+    public class ShakerComponent : MonoBehaviour
+    {
+        private Vector3 m_originalLocalPosition;
+        private Coroutine m_shakeCoroutine;
+        
+        public void Shake(float intensity = 0.5f, float duration = 0.15f)
+        {
+            if (m_shakeCoroutine != null)
+            {
+                StopCoroutine(m_shakeCoroutine);
+                transform.localPosition = m_originalLocalPosition;
+            }
+
+            m_shakeCoroutine = StartCoroutine(ShakeRoutine(intensity, duration));
+        }
+
+        private IEnumerator ShakeRoutine(float intensity, float duration)
+        {
+            m_originalLocalPosition = transform.localPosition;
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                float normalizedTime = elapsed / duration;
+                float damping = 1f - normalizedTime;
+                damping *= damping; 
+
+                float x = Random.Range(-1f, 1f) * intensity * damping;
+                float y = Random.Range(-1f, 1f) * intensity * damping;
+
+                transform.localPosition = m_originalLocalPosition + new Vector3(x, y, 0f);
+
+                elapsed += Time.unscaledDeltaTime; 
+                yield return null;
+            }
+
+            transform.localPosition = m_originalLocalPosition;
+            m_shakeCoroutine = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerFeedbackComponent.cs
@@ -0,0 +1,93 @@
+﻿using Game.Camera;
+using Game.Level;
+using Game.Utils;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerFeedbackComponent : MonoBehaviour
+    {
+        [Header("Components References")]
+        [SerializeField] private ShakerComponent m_cameraShake;
+        [SerializeField] private LevelManager m_levelManager;
+        [SerializeField] private ParticleSystem m_colorDust;
+        [SerializeField] private ParticleSystem m_cubeParticles;
+        
+        [SerializeField] private PlayerGroundDetector m_groundDetector;
+        [SerializeField] private PlayerMovement m_playerMovement;
+        [SerializeField] private PlayerColorHandler m_playerColorHandler;
+
+        [Header("Settings")]
+        [SerializeField] private float m_playerDeathCameraShakeIntensity = 0.5f; 
+        [SerializeField] private float m_playerDeathCameraShakeDuration = 0.2f;
+        [SerializeField] private float m_cubeParticlesLandVelocityThreshold = -24f;
+        
+        private Color m_blueColor;
+        private Color m_yellowColor;
+        
+        private void Awake()
+        {
+            ColorUtility.TryParseHtmlString(Constants.BLUE_COLOR_HEX, out var blueColor);
+            ColorUtility.TryParseHtmlString(Constants.YELLOW_COLOR_HEX, out var yellowColor);
+            
+            m_blueColor = blueColor;
+            m_yellowColor = yellowColor;
+            
+            m_levelManager.OnPlayerJustDied += () =>
+            {
+                PlayCubeParticles(m_playerColorHandler.CurrentColor);
+                m_cameraShake.Shake(m_playerDeathCameraShakeIntensity, m_playerDeathCameraShakeDuration);
+            };
+
+            if (!m_groundDetector) m_groundDetector = GetComponent<PlayerGroundDetector>();
+            if (!m_playerMovement) m_playerMovement = GetComponent<PlayerMovement>();
+            if (!m_playerColorHandler) m_playerColorHandler = GetComponent<PlayerColorHandler>();
+
+            if (m_playerColorHandler)
+            {
+                m_playerColorHandler.OnColorSwitched += newColor =>
+                {
+                    ModifyParticlesColor(m_colorDust, newColor);
+                };
+            }
+
+            if (m_groundDetector != null)
+            {
+                m_groundDetector.OnGroundDetected += () =>
+                {
+                    if (m_playerMovement.Velocity.y < m_cubeParticlesLandVelocityThreshold)
+                    {
+                        PlayCubeParticles(m_playerColorHandler.CurrentColor);    
+                    }
+                    
+                };
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            bool shouldEnableColorDustParticles = m_groundDetector.IsGrounded || m_playerMovement.IsJumping;
+
+            if (shouldEnableColorDustParticles && !m_colorDust.isPlaying)
+            {
+                m_colorDust.Play();
+            }
+            else if (!shouldEnableColorDustParticles && m_colorDust.isPlaying)
+            {
+                m_colorDust.Stop();
+            }
+        }
+
+        private void PlayCubeParticles(PlatformColor platformColor)
+        {
+            ModifyParticlesColor(m_cubeParticles, platformColor);
+            m_cubeParticles.Play(true);
+        }
+
+        private void ModifyParticlesColor(ParticleSystem particle, PlatformColor platformColor)
+        {
+            ParticleSystem.MainModule mainModule = particle.main;
+            mainModule.startColor = platformColor == PlatformColor.Blue ? m_blueColor : m_yellowColor;
+        }
+    }
+}
