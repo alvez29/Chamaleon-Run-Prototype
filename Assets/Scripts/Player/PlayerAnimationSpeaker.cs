ea9199173977fbb8c0efb86b6fe7575e1cd0a7da
Álvaro Martos <49306506+alvez29@users.noreply.github.com>
2026-05-29 01:22:43 +0200

    Implement PlayerAnimationSpeaker and relocate animator clips
    Create PlayerAnimationSpeaker to dispatch callback events directly from animator state clips. Relocate character animation assets into a dedicated Player/Animation folder subdirectory.

---
+++ Assets/Scripts/Player/PlayerAnimationSpeaker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerAnimationSpeaker : MonoBehaviour
+    {
+        [SerializeField] private PlayerAudioOrchestrator m_playerAudioOrchestrator;
+
+        private void OnStep()
+        {
+            m_playerAudioOrchestrator.PlayStepSound();
+        }
+    }
+}
